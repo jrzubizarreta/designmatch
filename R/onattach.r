fedf6a82f8998ced13a66e88df996367e6abd447
Noah Greifer <noah.greifer@gmail.com>
2026-02-13 15:46:23 -0500

    Doc updates

--- R/onattach.r
+++ R/onattach.r
@@ -1,6 +1,3 @@
-.onAttach <- 
-  function(libname, pkgname) {
-    packageStartupMessage("\nThank you for using our package! To acknowledge our work, please cite the package as: \n")
-    packageStartupMessage(" Zubizarreta, Jose R., Kilcioglu, Cinar, Vielma, Juan P., and Cohn, Eric R. (2023). designmatch: Matched Samples that are Balanced and Representative by Design.")
-    packageStartupMessage(" R package version 0.5.2. https://cran.r-project.org/web/packages/designmatch/ \n")
-  }
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage("Thank you for using our package! To acknowledge our work, please cite the package using `citation(\"designmatch\")`")
+}
